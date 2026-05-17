dacf4ab724041ef6e70282a8c0c3b5a56f2a511a
Betzaida Belén <1725111012@utectulancingo.edu.mx>
2026-05-17 17:27:52 -0600

    Update UNIDEH.py

--- ejercicios/semana_02/UNIDEH.py
+++ ejercicios/semana_02/UNIDEH.py
@@ -1,3 +1,12 @@
 class Universidad:
   def__init__(self, logo, oferta_educativa, localidad, sistema_informativo, modalidad, servicios, ubicacion, talleres, cantidad_salones, rector):
-  
+  self.logo = logo
+  self.oferta_educativa = oferta_educativa
+  self.localidad = localidad
+  self.sistema_informativo = sistema_informativo
+  self.modalidad = modalidad
+  self.logo = logo
+  self.logo = logo
+  self.logo = logo
+  self.logo = logo
+  self.logo = logo
