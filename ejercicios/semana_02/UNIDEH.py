ce9b3a8d7b667d83b0847b41f40a309a9d399d1b
Betzaida Belén <1725111012@utectulancingo.edu.mx>
2026-05-17 17:52:36 -0600

    Update UNIDEH.py
    Creación de class de la Universidad Digital de  Hidalgo

--- ejercicios/semana_02/UNIDEH.py
+++ ejercicios/semana_02/UNIDEH.py
@@ -1,12 +1,27 @@
 class Universidad:
   def__init__(self, logo, oferta_educativa, localidad, sistema_informativo, modalidad, servicios, ubicacion, talleres, cantidad_salones, rector):
+  
   self.logo = logo
   self.oferta_educativa = oferta_educativa
   self.localidad = localidad
   self.sistema_informativo = sistema_informativo
   self.modalidad = modalidad
-  self.logo = logo
-  self.logo = logo
-  self.logo = logo
-  self.logo = logo
-  self.logo = logo
+  self.servicios = servicios
+  self.ubicacion = ubicacion
+  self.talleres = talleres
+  self.cantidad_salones = cantidad_salones
+  self.rector = rector
+  
+  print("Logotipo de la Universidad: {self.logo}")
+  print("Oferta educativa: {self.oferta_educativa}")
+  print("Localidad: {self.localidad}")
+  print("Sitema informativo: {self.sistema_informativo}")
+  print("Modalidad: {self.modalidad}")
+  print("Servicios: {self.servicios}")
+  print("Ubicación: {self.ubicacion}")
+  print("Talleres: {self.talleres}")
+  print("Cantidad de salones: {self.cantidad_salones}")
+  print("Rector: {self.rector}")
+
+unideh = Universidad("logo.jpg", "Ing. Software, Turismo, Gestión Empresarial", "San Miguel", "CADU", "Virtual", "Biblioteca Virtual", "Santa Catarina", None, None, "Octavio Castillo")
+                     
