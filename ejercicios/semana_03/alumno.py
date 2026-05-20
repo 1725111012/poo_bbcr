39e550b8a2703ac55c24d343da3cdeddc27fd626
Betzaida Belén <1725111012@utectulancingo.edu.mx>
2026-05-20 08:36:57 +0000

    UPDATE

--- ejercicios/semana_03/alumno.py
+++ ejercicios/semana_03/alumno.py
@@ -1,43 +1,44 @@
-class Coche:
-  def __init__(self, tipo_rines, tipo_llantas, tipo_volante, automatico_estandar, asientos, numero_modelo, asistencia, marca, capacidad_tanque, color):
+class Alumno:
+  def __init__(self, matricula, nombre, carrera_cursa, institucion_cursa, grado, edad, material, materias, horario, becas):
  
-        self.tipo_rines = tipo_rines
-        self.tipo_llantas = tipo_llantas
-        self.tipo_volante = tipo_volante
-        self.automatico_estandar = automatico_estandar
-        self.asientos = asientos
-        self.numero_modelo = numero_modelo
-        self.asistencia = asistencia
-        self.marca = marca
-        self.capacidad_tanque = capacidad_tanque
-        self.color = color
+        self.matricula = matricula
+        self.nombre = nombre
+        self.carrera_cursa = carrera_cursa
+        self.institucion_cursa = institucion_cursa
+        self.grado = grado
+        self.edad = edad
+        self.material = material
+        self.materias = materias
+        self.horario = horario
+        self.becas = becas
         
-        print("Tipo de rines: {self.tipo_rines}")
-        print("Tipo de llantas: {self.tipo_llantas}")
-        print("Tipo de volante: {self.tipo_volante}")
-        print("Cantidad de asientos: {self.automatico_estandar}")
-        print("Número de modelo: {self.asientos}")
-        print("Asistencia: {self.numero_modelo}")
-        print("Marca: {self.asistencia}")
-        print("Capacidad del tanque: {self.marca}")
-        print("Color: {self.capacidad_tanque}")
-        print("Automática o estándar: {self.color}")
+        print("Matricula del estudiante: {self.matricula}")
+        print("Nombre del estudiante: {self.nombre}")
+        print("Carrera que esta cursando: {self.carrera_cursa}")
+        print("Institución donde cursa su carrera: {self.institucion_cursa}")
+        print("Grado o nivel del estudiante: {self.grado}")
+        print("Edad del alumno: {self.edad}")
+        print("Material que el alumno usa: {self.material}")
+        print("Materias que cursa el alumno: {self.materias}")
+        print("Horario de asistencia del alumno: {self.horario}")
+        print("¿El alumno cuenta con beca?: {self.becas}")
 
-        def manejar (self):
-            print ("Manejar el coche")
-        def chocar (self):
-            print ("Cuidado al manejar, podría chocar")
-        def atropellar (self):
-            print ("Cuidado al manejar, no atropelle a nadie")
-        def trasladar (self):
-            print ("Trasladar usando un coche")
-        def viajar (self):
-            print ("Viajar en coche")
+        def estudiar (self):
+            print ("El alumno va a estudiar para un examen")
+        def aprender (self):
+            print ("El alumno va a aprender")
+        def cursar (self):
+            print ("El alumno cursa un grado")
+        def escribir (self):
+            print ("El alumno escribe")
+        def asistir (self):
+            print ("El alumno asiste a clases")
 
-nissan = Coche("De aluminio 16 pulgadas", "205/60 R16", "Forrado de piel", "Automatico", "5 asientos", "005", "Bolsas de aire, frenos", "Nissan", "47 litros", "Negro")
+alumna_utec = Alumno("1725111012", "Betzaida Belén", "TIC'S", "Unversidad Tecnológica de Tulancingo", "3er cuatrimestre", 
+                    "19 años", "Libreta, labtops, lapiceros", "Calculo Integral, Programacón Orientada a Objetos", "De 7:00 a 3:00 PM", None)
 
-nissan.manejar()
-nissan.chocar()
-nissan.atropellar()
-nissan.trasladar()
-nissan.viajar()
+alumna_utec.estudiar()
+alumna_utec.aprender()
+alumna_utec.cursar()
+alumna_utec.escribir()
+alumna_utec.asistir()
