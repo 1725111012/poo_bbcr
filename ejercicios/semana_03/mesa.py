db1ca4cbfcc31c7e2adc372cd4dc240681712c84
Betzaida Belén <1725111012@utectulancingo.edu.mx>
2026-05-20 07:03:24 +0000

    UPDATE

--- ejercicios/semana_03/mesa.py
+++ ejercicios/semana_03/mesa.py
@@ -1,44 +1,44 @@
-class Coche:
-  def __init__(self, tipo_rines, tipo_llantas, tipo_volante, automatico_estandar, asientos, numero_modelo, asistencia, marca, capacidad_tanque, color):
+class Mesa:
+  def __init__(self, soporte, superficie_plana, color, forma_base, material, detalles, peso, ensamble, ancho, dureza):
  
-  self.tipo_rines = tipo_rines
-  self.tipo_llantas = tipo_llantas
-  self.tipo_volante = tipo_volante
-  self.automatico_estandar = automatico_estandar
-  self.asientos = asientos
-  self.numero_modelo = numero_modelo
-  self.asistencia = asistencia
-  self.marca = marca
-  self.capacidad_tanque = capacidad_tanque
-  self.color = color
-   
-  print("Tipo de rines: {self.tipo_rines}")
-  print("Tipo de llantas: {self.tipo_llantas}")
-  print("Tipo de volante: {self.tipo_volante}")
-  print("Cantidad de asientos: {self.automatico_estandar}")
-  print("Número de modelo: {self.asientos}")
-  print("Asistencia: {self.numero_modelo}")
-  print("Marca: {self.asistencia}")
-  print("Capacidad del tanque: {self.marca}")
-  print("Color: {self.capacidad_tanque}")
-  print("Automática o estándar: {self.color}")
+        self.soporte = soporte
+        self.superficie_plana = superficie_plana
+        self.color = color
+        self.forma_base = forma_base
+        self.material = material
+        self.detalles = detalles
+        self.peso = peso
+        self.ensamble = ensamble
+        self.ancho = ancho
+        self.dureza = dureza
+        
+        print("Soporte de la mesa: {self.soporte}")
+        print("Tipo de superficie de la mesa y tamaño: {self.superficie_plana}")
+        print("Color de la mesa: {self.color}")
+        print("Forma de la base de la mesa: {self.forma_base}")
+        print("Material utilizado para la mesa: {self.material}")
+        print("Detalles de la mesa: {self.detalles}")
+        print("Peso de la mesa: {self.peso}")
+        print("Tipo de ensamblaje: {self.ensamble}")
+        print("Tamaño del ancho de la mesa: {self.ancho}")
+        print("Cantidad que soporta la mesa: {self.dureza}")
 
+        def limpiar (self):
+            print ("Limpiar la mesa")
+        def prestar (self):
+            print ("Prestar la mesa")
+        def cargar (self):
+            print ("Cargar la mesa")
+        def usar (self):
+            print ("Usar/usando la mesa")
+        def inclinar (self):
+            print ("Se puede inclinar la mesa")
 
-def manejar (self):
-  print ("Manejar el coche")
-def chocar (self):
-  print ("Cuidado al manejar, podría chocar")
-def atropellar (self):
-  print ("Cuidado al manejar, no atropelle a nadie")
-def trasladar (self):
-  print ("Trasladar usando un coche")
-def viajar (self):
-  print ("Viajar en coche")
+mesa_oficina = Mesa("4 patas metálicas", "Tablero rectangular de 120 x 160 cm", "Gris", "Forma de H", "Estrucutra de acero", 
+                    "Portavasos y ganchos para bolsas", "25 y 45 kg", "Con tornillos", "De 60 a 80 cm", "Soporta de 30 a 50 kg")
 
-nissan = Coche("De aluminio 16 pulgadas", "205/60 R16", "Forrado de piel", "Automatico", "5 asientos", "005", "Bolsas de aire, frenos", "Nissan", "47 litros", "Negro")
-
-nissan.manejar()
-nissan.chocar()
-nissan.atropellar()
-nissan.trasladar()
-nissan.viajar()
+mesa_oficina.limpiar()
+mesa_oficina.prestar()
+mesa_oficina.cargar()
+mesa_oficina.usar()
+mesa_oficina.inclinar()
