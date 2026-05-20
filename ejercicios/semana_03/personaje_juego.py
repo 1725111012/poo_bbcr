4aa9a90a4bd97b9c952e27740e0c656353fd2676
Betzaida Belén <1725111012@utectulancingo.edu.mx>
2026-05-20 07:59:34 +0000

    UPDATE

--- ejercicios/semana_03/personaje_juego.py
+++ ejercicios/semana_03/personaje_juego.py
@@ -1,44 +1,45 @@
-class Mesa:
-  def __init__(self, soporte, superficie_plana, color, forma_base, material, detalles, peso, ensamble, ancho, dureza):
+class PersonajeJuego:
+  def __init__(self, nombre, personalidad, rol, juego, genero, raza, vida_hp, poderes, voz, debilidad):
  
-        self.soporte = soporte
-        self.superficie_plana = superficie_plana
-        self.color = color
-        self.forma_base = forma_base
-        self.material = material
-        self.detalles = detalles
-        self.peso = peso
-        self.ensamble = ensamble
-        self.ancho = ancho
-        self.dureza = dureza
+        self.nombre = nombre
+        self.personalidad = personalidad 
+        self.rol = rol 
+        self.juego = juego
+        self.genero = genero
+        self.raza = raza
+        self.vida_hp = vida_hp 
+        self.poderes = poderes
+        self.voz = voz
+        self.debilidad = debilidad
         
-        print("Soporte de la mesa: {self.soporte}")
-        print("Tipo de superficie de la mesa y tamaño: {self.superficie_plana}")
-        print("Color de la mesa: {self.color}")
-        print("Forma de la base de la mesa: {self.forma_base}")
-        print("Material utilizado para la mesa: {self.material}")
-        print("Detalles de la mesa: {self.detalles}")
-        print("Peso de la mesa: {self.peso}")
-        print("Tipo de ensamblaje: {self.ensamble}")
-        print("Tamaño del ancho de la mesa: {self.ancho}")
-        print("Cantidad que soporta la mesa: {self.dureza}")
+        print("Nombre de los personaje: {self.nombre}")
+        print("Personalidad del personaje: {self.personalidad}")
+        print("Rol que cumple en el juego: {self.rol}")
+        print("Juego donde esta el personaje: {self.juego}")
+        print("Genéro del personaje: {self.genero}")
+        print("Raza del personaje: {self.raza}")
+        print("Vida del personaje: {self.vida_hp}")
+        print("Poderes que tiene el personaje: {self.poderes}")
+        print("¿Tiene voz el personaje?: {self.voz}")
+        print("Debilidad del personaje: {self.debilidad}")
 
-        def limpiar (self):
-            print ("Limpiar la mesa")
-        def prestar (self):
-            print ("Prestar la mesa")
-        def cargar (self):
-            print ("Cargar la mesa")
-        def usar (self):
-            print ("Usar/usando la mesa")
-        def inclinar (self):
-            print ("Se puede inclinar la mesa")
+        def caminar (self):
+            print ("El personaje puede caminar")
+        def teletransportarse (self):
+            print ("El personaje puede teletransportarse")
+        def curarse (self):
+            print ("El personaje puede curarse")
+        def interactuar (self):
+            print ("Se puede interactuar con el personaje")
+        def morir (self):
+            print ("Cuidado, el personaje puede morir")
 
-mesa_oficina = Mesa("4 patas metálicas", "Tablero rectangular de 120 x 160 cm", "Gris", "Forma de H", "Estrucutra de acero", 
-                    "Portavasos y ganchos para bolsas", "25 y 45 kg", "Con tornillos", "De 60 a 80 cm", "Soporta de 30 a 50 kg")
+sans = PersonajeJuego("Sans el esqueleto", "Flojo, sarcastico y bromista", "NPC/Jefe final", "Undertale", "Maculino", "Monstruo, esqueleto", "1 HP", 
+                        "Teletransportación, control de gravedad de las almas, huesos, gaster blasters", None, "Solo tiene 1 HP, muere de un golpe")
 
-mesa_oficina.limpiar()
-mesa_oficina.prestar()
-mesa_oficina.cargar()
-mesa_oficina.usar()
-mesa_oficina.inclinar()
+sans.caminar()
+sans.teletransportarse()
+sans.curarse()
+sans.interactuar()
+sans.morir()
+
